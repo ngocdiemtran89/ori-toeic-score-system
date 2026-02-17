bcafb764981ccb99ae5eeb7783ac38dd997bdbcd
ngocdiemtran89 <Tranngocdiem89@gmail.com>
2026-02-17 12:56:36 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="vi">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Great+Vibes&family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=Dancing+Script:wght@400;700&family=Cormorant+Garamond:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
